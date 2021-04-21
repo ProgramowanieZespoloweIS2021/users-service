a873f3dc7e6c2c8a4a800aa14c9bae2f7200b3cb
jakubkowalski <kubabartek1@op.pl>
2021-04-21 21:43:12 +0200

    delete unnecessary bounding into interfaces

--- src/main/java/com/eszop/usersservice/user/service/UserService.java
+++ src/main/java/com/eszop/usersservice/user/service/UserService.java
@@ -2,17 +2,43 @@ package com.eszop.usersservice.user.service;
 
 import com.eszop.usersservice.user.model.User;
 import com.eszop.usersservice.user.model.UserId;
+import com.eszop.usersservice.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface UserService {
-    void save(User user);
+@Service
+@RequiredArgsConstructor
+public class UserService {
 
-    User findByEmail(String username);
-    
-    User findById(Long id);
+    private final UserRepository userRepository;
 
-    List<User> findAll();
 
-    void deleteUserById(UserId id);
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+
+    public User findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+
+    public void deleteUserById(UserId id) {
+        User user = userRepository.getOne(id);
+        userRepository.delete(user);
+    }
+
+
 }
