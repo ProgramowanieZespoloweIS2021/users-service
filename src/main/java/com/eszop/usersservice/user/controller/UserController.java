2a944190fee4a816e3559ee5a9d5a0b9e4dc2ba8
jakubkowalski <kubabartek1@op.pl>
2021-05-31 18:30:33 +0200

    do not return 200 if user not found

--- src/main/java/com/eszop/usersservice/user/controller/UserController.java
+++ src/main/java/com/eszop/usersservice/user/controller/UserController.java
@@ -6,11 +6,14 @@ import com.eszop.usersservice.user.model.User;
 import com.eszop.usersservice.user.model.UserId;
 import com.eszop.usersservice.user.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -44,12 +47,18 @@ public class UserController {
 
     @GetMapping
     @ResponseBody
-    public List<UserDTO> getUsers(@RequestParam(required = false) String email){
+    public ResponseEntity<?> getUsers(@RequestParam(required = false) String email){
         //TODO enable calls only by auth service
         if (email != null && !email.isEmpty()) {
-            return Collections.singletonList(UserDTOMapper.map(userService.findByEmail(email)));
+            return Optional.ofNullable(userService.findByEmail(email))
+                    .map(user -> Collections.singletonList(UserDTOMapper.map(user)))
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
         }
-        return userService.findAll().stream().map(UserDTOMapper::map).collect(Collectors.toList());
+        return Optional.ofNullable(userService.findAll())
+                .map(users -> users.stream().map(user -> UserDTOMapper.map(user)).collect(Collectors.toList()))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 }
